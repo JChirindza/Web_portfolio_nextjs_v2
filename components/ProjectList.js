f2b69cc252895a189af9a1764af7272f22c153e3
João Chirindza <johnchirindza@gmail.com>
2021-11-27 16:07:07 +0200

    Update projectList

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -4,7 +4,7 @@ import projectStyles from '../styles/Project.module.css'
 const ProjectList = ({projects}) => {
     return (
 
-        <section className='s2' id="projects">
+        <section className='s1' id="projects">
             <div className='main_container'>
                 <h3 className='text_center'>SOME OF MY PROJECTS</h3>
                 <div>
