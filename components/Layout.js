e525150455f7bc68dc2631410469ae8307797700
João Chirindza <johnchirindza@gmail.com>
2021-11-27 16:06:33 +0200

    Update Layout.js
    import Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Nav from './Nav'
 import Meta from './Meta'
-import Header from './Header'
+import Footer from './Footer'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
@@ -10,10 +10,10 @@ const Layout = ({ children }) => {
             <Nav />
             <div className={styles.container}>
                 <main className={styles.main}>
-                    <Header />    
                     {children}
                 </main>
             </div>
+            <Footer />
         </>
     )
 }
