459530f1f5b9fcf6275c9e918ccc73c033ce23c7
João Chirindza <johnchirindza@gmail.com>
2021-11-27 16:07:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,24 @@
 import { server } from '../config'
-import ArticleList from '../components/ArticleList'
+import ProjectList from '../components/ProjectList'
 import Meta from '../components/Meta'
-export default function Home({ articles }) {
+import HomePage from './home'
+export default function Home({ projects }) {
   return (
     <div>
       <Meta />
-      <ArticleList articles={articles} />
+      <HomePage />
+      <ProjectList projects = {projects} />
     </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`)
-  const articles = await res.json()
+  const res = await fetch(`${server}/api/projects`)
+  const projects = await res.json()
 
   return {
     props: {
-      articles,
+      projects,
     },
   }
 }
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-//   const articles = await res.json()
-
-//   return {
-//     props: {
-//       articles,
-//     },
-//   }
-// }
