6451fd8e40ce686656b69ea04f3ce1e7f7d96fc9
João Chirindza <johnchirindza@gmail.com>
2022-02-21 13:57:54 +0200

    update pages

--- components/Nav.js
+++ components/Nav.js
@@ -1,79 +1,91 @@
-import Link from 'next/link'
-import navStyles from '../styles/Nav.module.css';
+import Link from "next/link";
+import navStyles from "../styles/Nav.module.css";
 
 const Nav = () => {
     return (
         <header className={`${navStyles.s0} ${navStyles.header}`}>
             <div className={navStyles.main_container}>
-                <div className={`${navStyles.row} ${navStyles.v_center} ${navStyles.header_wrapper}`}>
-                    <div className={`${navStyles.header_item} ${navStyles.item_left}`}>
+                <div
+                    className={`${navStyles.row} ${navStyles.v_center} ${navStyles.header_wrapper}`}
+                >
+                    <div
+                        className={`${navStyles.header_item} ${navStyles.item_left}`}
+                    >
                         <div className={navStyles.logo}>
-                            <Link href='/'>JChirindza</Link>
+                            <Link href="/">JChirindza</Link>
                         </div>
                     </div>
 
-                    <div className={`${navStyles.header_item} ${navStyles.item_center}`}>
+                    <div
+                        className={`${navStyles.header_item} ${navStyles.item_center}`}
+                    >
                         <div className={navStyles.menu_overlay}></div>
 
                         <nav className={navStyles.menu}>
                             <div className={navStyles.mobile_menu_logo}>
-                                <div className={`${navStyles.header_item} ${navStyles.item_left}`}>
+                                <div
+                                    className={`${navStyles.header_item} ${navStyles.item_left}`}
+                                >
                                     <div className={navStyles.logo}>
-                                        <Link href='/'>JChirindza</Link>
+                                        <Link href="/">JChirindza</Link>
                                     </div>
                                 </div>
                             </div>
 
                             <div className={navStyles.mobile_menu_head}>
                                 <div className={navStyles.go_back}></div>
-                                <div className={navStyles.current_menu_title}></div>
-                                <div className={navStyles.mobile_menu_close}>&times;</div>
+                                <div
+                                    className={navStyles.current_menu_title}
+                                ></div>
+                                <div className={navStyles.mobile_menu_close}>
+                                    &times;
+                                </div>
                             </div>
 
                             <ul className={navStyles.menu_main}>
                                 <li>
-                                    <Link href='/#' className={navStyles.active}>Home</Link>
+                                    <Link
+                                        href="/#"
+                                        className={navStyles.active}
+                                    >
+                                        Home
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href='/#about' className={navStyles.active}>About</Link>
+                                    <Link
+                                        href="/#about"
+                                        className={navStyles.active}
+                                    >
+                                        About
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href='/#projects' className={navStyles.active}>Projects</Link>
+                                    <Link
+                                        href="/#projects"
+                                        className={navStyles.active}
+                                    >
+                                        Projects
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href='/#contact'>Contact</Link>
-                                </li>
-                                <li className={navStyles.menu_item_has_children}>
-                                    <Link href=''>Settings</Link>
-                                    {/* <a href="javascript:void(0);">Settings</a> */}
-                                    <div className={`${navStyles.sub_menu} ${navStyles.single_column_menu}`}>
-                                        <label> Settings will be saved for your next visit</label>
-                                        
-                                        <ul>
-                                            <p id="settings-note"> Change theme</p>
-                                            <div id="theme-options-wrapper">
-                                                <div data-mode="light" id="light-mode" className={navStyles.theme_dot}></div>
-                                                <div data-mode="blue" id="blue-mode" className={navStyles.theme_dot}></div>
-                                                <div data-mode="green" id="green-mode" className={navStyles.theme_dot}></div>
-                                                <div data-mode="purple" id="purple-mode" className={navStyles.theme_dot}></div>
-                                            </div>
-                                        </ul>
-                                    </div>
+                                    <Link href="/#contact">Contact</Link>
                                 </li>
                             </ul>
                         </nav>
                     </div>
 
-                    <div className={`${navStyles.header_item} ${navStyles.item_right}`}>
+                    <div
+                        className={`${navStyles.header_item} ${navStyles.item_right}`}
+                    >
                         {/* mobile menu trigger */}
                         <div className={navStyles.mobile_menu_trigger}>
                             <span></span>
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
         </header>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
--- pages/home.js
+++ pages/home.js
@@ -1,106 +1,153 @@
-import homeStyles from '../styles/Home.module.css'
+import homeStyles from "../styles/Home.module.css";
 
 const Home = () => {
-    const x = 2
+    const x = 2;
     return (
         <>
-        <section className='s1 fullscreen'>
-            <div className='main_container'>
-                <div className='greeting_wrapper'>
-                    <h1>Hi, I'm <label className={homeStyles.jc_name}>João Chirindza</label></h1>
-                </div>
-
-                <div className='intro_wrapper'>
-                    <div className="left_column">
-                        <img id="profile_pic" src="../images/john.jpg" />
-                        <h4 className={homeStyles.pic_description}><label>I'm a Web Developer</label></h4>
-                        <p id="settings-note"><i className="fas fa-info-circle"></i> Available for new opportunities</p>
+            <section className="s1">
+                <div className="main_container">
+                    <div className="greeting_wrapper">
+                        <h1>
+                            Hi, I'm{" "}
+                            <label className={homeStyles.jc_name}>
+                                João Chirindza
+                            </label>
+                        </h1>
                     </div>
 
-                    <div className='right_column'>
-                        <div id='preview-shadow'>
-                            <div id="preview">
-                                <div id="corner-tl" className='corner'></div>
-                                <div id="corner-tr" className='corner'></div>
-                                <h3>What I Do</h3>
-                                <p>Web Developer with experience in building, integrating and testing web applications. Backend creation with PHP or Node.js;</p>
-                                <div id="corner-br" className='corner'></div>
-                                <div id="corner-bl" className='corner'></div>
+                    <div className="intro_wrapper">
+                        <div className="left_column">
+                            <img id="profile_pic" src="../images/john.jpg" />
+                            <h4 className={homeStyles.pic_description}>
+                                <label>I'm a Web Developer</label>
+                            </h4>
+                            <p id="settings-note">
+                                <i className="fas fa-info-circle"></i> Available
+                                for new opportunities
+                            </p>
+                        </div>
+
+                        <div className="right_column">
+                            <div id="preview-shadow">
+                                <div id="preview">
+                                    <div
+                                        id="corner-tl"
+                                        className="corner"
+                                    ></div>
+                                    <div
+                                        id="corner-tr"
+                                        className="corner"
+                                    ></div>
+                                    <h3>What I Do</h3>
+                                    <p>
+                                        Web Developer with experience in
+                                        building, integrating and testing web
+                                        applications. Backend creation with PHP
+                                        or Node.js;
+                                    </p>
+                                    <div
+                                        id="corner-br"
+                                        className="corner"
+                                    ></div>
+                                    <div
+                                        id="corner-bl"
+                                        className="corner"
+                                    ></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section className='s2' id="about">
-            <div className='main_container'>
-                <h3 className='text_center'>MORE ABOUT ME</h3>
-                <div className='about_wrapper'>
-                    <div className='text_center'>
-                        <p>I'm a web developer based in Maputo, Mozambique and Computer Engineering Student.</p>
+            <section className="s2" id="about">
+                <div className="main_container">
+                    <h3 className="text_center">MORE ABOUT ME</h3>
+                    <div className="about_wrapper">
+                        <div className="text_center">
+                            <p>
+                                I'm a web developer based in Maputo, Mozambique
+                                and Computer Engineering Student.
+                            </p>
 
-                        <p>When I'm not coding/working on some project, probably I'm learning more about the Web technologies, talking/writing about technology or exchanging impressions and experiences.</p>
-                    </div>
+                            <p>
+                                When I'm not coding/working on some project,
+                                probably I'm learning more about the Web
+                                technologies, talking/writing about technology
+                                or exchanging impressions and experiences.
+                            </p>
+                        </div>
 
-                    <div className='about_me'>
+                        <div className="about_me">
+                            <div className="expertises">
+                                <h4 className="text_center">Top expertise</h4>
 
-                        <div className='expertises'>
-                            <h4 className='text_center'>Top expertise</h4>
-                            
-                            <p className='text_center'>Here are some technologies I've been working with recently:</p>
+                                <p className="text_center">
+                                    Here are some technologies I've been working
+                                    with recently:
+                                </p>
 
-                            <div id='skills'>
-                                <ul>
-                                    <li className='skill_title'>Languages 1</li>
-                                    <li>- PHP</li>
+                                <div id="skills">
                                     <ul>
-                                        <li>- Laravel</li>
+                                        <li className="skill_title">
+                                            Languages 1
+                                        </li>
+                                        <li>- PHP</li>
+                                        <ul>
+                                            <li>- Laravel</li>
+                                        </ul>
+                                        <li>- JavaScript</li>
+                                        <ul>
+                                            <li>- React JS</li>
+                                            <li>- Node.js</li>
+                                            <li>- jQuery</li>
+                                        </ul>
                                     </ul>
-                                    <li>- JavaScript</li>
-                                    <ul>
-                                        <li>- React JS</li>
-                                        <li>- Node.js</li>
-                                        <li>- jQuery</li>
-                                    </ul>
-                                </ul>
 
-                                <ul>
-                                    <li className='skill_title'>Languages 2</li>
-                                    <li>- JAVA</li>
                                     <ul>
-                                        <li>- Spring Boot</li>
+                                        <li className="skill_title">
+                                            Languages 2
+                                        </li>
+                                        <li>- JAVA</li>
+                                        <ul>
+                                            <li>- Spring Boot</li>
+                                        </ul>
+                                        <li>- C#</li>
+                                        <ul>
+                                            <li>- .NET</li>
+                                        </ul>
                                     </ul>
-                                    <li>- C#</li>
                                     <ul>
-                                        <li>- .NET</li>
+                                        <li className="skill_title">
+                                            Database/Hosting
+                                        </li>
+                                        <li>- MySQL</li>
+                                        <li>- SQL Sever</li>
+                                        <li>- AWS</li>
+                                        <li>- Heroku</li>
                                     </ul>
-                                </ul>
-                                <ul>
-                                    <li className='skill_title'>Database/Hosting</li>
-                                    <li>- MySQL</li>
-                                    <li>- SQL Sever</li>
-                                    <li>- AWS</li>
-                                    <li>- Heroku</li>
-                                </ul>
-                                <ul>
-                                    <li className='skill_title'>Other tech's</li>
-                                    <li>- WordPress</li>
-                                    <li>- Git and Github</li>
-                                </ul>
+                                    <ul>
+                                        <li className="skill_title">
+                                            Other tech's
+                                        </li>
+                                        <li>- WordPress</li>
+                                        <li>- Git and Github</li>
+                                    </ul>
+                                </div>
+
+                                <p className="text_center">
+                                    Nowadays focusing on PHP + JavaScript:{" "}
+                                    <a target="_blank" href="resume.pdf">
+                                        Download Resume
+                                    </a>
+                                </p>
                             </div>
-
-                            <p className='text_center'>Nowadays focusing on PHP + JavaScript: <a target="_blank" href="resume.pdf">Download Resume</a></p>
-
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        
+            </section>
         </>
+    );
+};
 
-    )
-}
-
-export default Home
+export default Home;
