406fb4f0de519caf4e567f036e2c63d12a292eee
João Chirindza <johnchirindza@gmail.com>
2022-03-12 10:17:09 +0200

    ProjectList.js Update | UI reset

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -65,8 +65,12 @@ const ProjectList = ({ allProjects: allProjects }) => {
                                                             projectStyles.post_title
                                                         }
                                                     >
-                                                        {post.frontmatter.title}{" "}
-                                                        &rarr;
+                                                        <span>
+                                                            {
+                                                                post.frontmatter
+                                                                    .title
+                                                            }
+                                                        </span>
                                                     </h6>
                                                 </Link>
                                                 <p
@@ -100,7 +104,6 @@ const ProjectList = ({ allProjects: allProjects }) => {
                                             }
                                         >
                                             <a
-                                                href="projects.html"
                                                 data-toggle="tooltip"
                                                 title="View More"
                                             >
@@ -124,96 +127,6 @@ const ProjectList = ({ allProjects: allProjects }) => {
                     </div>
                 </div>
             </section>
-            <style jsx>
-                {`
-                    margin-bottom: 0;
-                    a:hover {
-                        opacity: 1;
-                    }
-                    a:hover li div.hero_image img {
-                        opacity: 0.8;
-                        transition: opacity 0.3s ease;
-                    }
-                    a:hover li .project__info h2,
-                    a:hover li .project__info h3,
-                    a:hover li .project__info p {
-                        transform: translateX(10px);
-                        transition: transform 0.5s ease-out;
-                    }
-                    .hero_image {
-                        width: 100%;
-                        height: 33vh;
-                        overflow: hidden;
-                        background-color: #000;
-                    }
-                    .hero_image img {
-                        object-fit: cover;
-                        object-position: 50% 50%;
-                        opacity: 1;
-                        transition: opacity 0.3s ease;
-                        min-height: 100%;
-                    }
-                    .project__info {
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: center;
-                        padding: 1.5rem 1.25rem;
-                        transform: translateX(0px);
-                        transition: transform 0.3s ease-in;
-                        border-bottom: 1px solid #ebebeb;
-                    }
-                    .project__info h2,
-                    .project__info h3,
-                    .project__info p {
-                        transform: translateX(0px);
-                        transition: transform 0.5s ease-out;
-                    }
-                    li {
-                        opacity: inherit;
-                        display: flex;
-                        justify-content: center;
-                        flex-direction: column;
-                        min-height: 38vh;
-                        margin-bottom: 0;
-                    }
-                    h2 {
-                        margin-bottom: 0.5rem;
-                    }
-                    h3 {
-                        margin-bottom: 1rem;
-                    }
-                    p {
-                        max-width: 900px;
-                    }
-                    @media (min-width: 768px) {
-                        li {
-                            min-height: 250px;
-                            height: 33.333vh;
-                            flex-direction: row;
-                        }
-                        .hero_image {
-                            height: 100%;
-                        }
-                        .hero_image img {
-                            min-width: 100%;
-                            height: 100%;
-                            width: auto;
-                            min-height: 0;
-                        }
-                        .project__info {
-                            min-width: 70%;
-                        }
-                    }
-                    @media (min-width: 1280px) {
-                        .project__info {
-                            padding: 3rem;
-                        }
-                        h3 {
-                            margin-bottom: 1.2rem;
-                        }
-                    }
-                `}
-            </style>
         </>
     );
 };
