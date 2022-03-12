879591c44bc0a361254b9e42af0c814fcdcdf434
João Chirindza <johnchirindza@gmail.com>
2022-03-12 10:27:16 +0200

    Add new configurations for Markdown | Raw Loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    webpack: function(config) {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader',
+        })
+        return config
+    },
 }
