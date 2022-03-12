940e944167efe30598e62ba15646e3fb292b2dab
João Chirindza <johnchirindza@gmail.com>
2022-03-12 10:42:51 +0200

    Import font-awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Layout from '../components/layout'
-import '../styles/globals.css'
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import "../public/font-awesome/css/all.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
