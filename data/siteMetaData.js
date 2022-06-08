98374f33a23b0ee023a51002e969e82414d654c4
João Chirindza <johnchirindza@gmail.com>
2022-06-08 20:54:19 +0200

    Create siteMetadata.js file

---
+++ data/siteMetaData.js
@@ -0,0 +1,24 @@
+const siteMetadata = {
+    title: 'João Chirindza Web Portfolio',
+    headerTitle: 'Web portfolio',
+    description: 'Personal website to promote my work and show a little about who I am and what I do.',
+    author: 'João Chirindza',
+    github: 'https://github.com/jchirindza/',
+    twitter: 'https://twitter.com/johnchirindza',
+    linkedin: 'https://www.linkedin.com/in/jchirindza',
+    language: 'en-us',
+    siteUrl: 'https://www.jchirindza.vercel.app',
+    siteRepo: 'https://github.com/JChirindza/Web_portfolio_nextjs_v2',
+    image: '/images/john.jpg',
+    email: 'johnchirindza@gmail.com',
+
+    locale: 'en-US',
+    analytics: {
+        // supports plausible, simpleAnalytics or googleAnalytics
+        plausibleDataDomain: false, // e.g. tailwind-nextjs-starter-blog.vercel.app
+        simpleAnalytics: false, // true or false
+        googleAnalyticsId: 'UA-100201704-1', // e.g. UA-000000-2 or G-XXXXXXX
+    }
+}
+
+module.exports = siteMetadata
