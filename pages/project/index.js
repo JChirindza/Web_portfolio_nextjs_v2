358c5e02233fa4af26892511404811684ae9998c
João Chirindza <johnchirindza@gmail.com>
2022-07-02 19:49:05 +0200

    Add new pages

---
+++ pages/project/index.js
@@ -0,0 +1,20 @@
+const projects = () => {
+    return (
+        <>
+            <section className="s1">
+                <div className="main_container">
+                    <h2>Projects</h2>
+
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                </div>
+            </section>
+
+            <style jsx>{``}</style>
+        </>
+    );
+};
+
+export default projects;
