24c8e3cd63d080676d75b1b075be783fa6f4bb6a
João Chirindza <johnchirindza@gmail.com>
2022-10-13 01:49:38 +0200

    Set google analystics

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,42 @@ import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import "../public/font-awesome/css/all.min.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ThemeProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ThemeProvider>
+        <>
+            <ThemeProvider>
+                <Layout>
+                    <Script
+                        strategy="lazyOnload"
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                    />
+
+                    {/* <Script strategy="lazyOnload" id="g-analystics">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                             function gtag(){dataLayer.push(arguments);}
+                             gtag('js', new Date());
+                           
+                             gtag('config', 'G-PXD9FXG39H'); 
+                        `}
+                    </Script> */}
+                    <Script strategy="lazyOnload" id="g-analystics">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                            page_path: window.location.pathname,
+                            });
+                        `}
+                    </Script>
+
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </>
     );
 }
 
