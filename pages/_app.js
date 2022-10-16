a957e5fa418855bb1c646d51d92d7b12cb092d7e
João Chirindza <johnchirindza@gmail.com>
2022-10-17 01:35:54 +0200

    Update _app.js file
    Google Analystics

--- pages/_app.js
+++ pages/_app.js
@@ -14,15 +14,6 @@ function MyApp({ Component, pageProps }) {
                         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
                     />
 
-                    {/* <Script strategy="lazyOnload" id="g-analystics">
-                        {`
-                            window.dataLayer = window.dataLayer || [];
-                             function gtag(){dataLayer.push(arguments);}
-                             gtag('js', new Date());
-                           
-                             gtag('config', 'G-PXD9FXG39H'); 
-                        `}
-                    </Script> */}
                     <Script strategy="lazyOnload" id="g-analystics">
                         {`
                             window.dataLayer = window.dataLayer || [];
